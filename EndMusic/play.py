48bc22270f9626fce718e6d1b7de638532295a8e
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-01 18:00:50 +0530

    Create play.py

---
+++ EndMusic/play.py
@@ -0,0 +1,12 @@
+from . import End, filters, MyHeartYashvi, BOT_USERNAME
+
+@End.on_message(filters.command(["play", f"play@{BOT_USERNAME}"]) & filters.group & ~filters.bot & ~filters.edited & ~filters.forwarded)
+async def play_func(_, m: MyHeartYashvi):
+    user_id = m.from_user.id
+    user_name = m.from_user.first_name
+    replied = m.reply_to_message
+    if m.sender_chat:
+        return await m.reply("Looks like you're an anonymous, try without anonymous 🤧")
+    if not user_id:
+        return
+    
