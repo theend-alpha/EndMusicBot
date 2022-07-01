fa6682320a095eafce6b1556482aefce75826563
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-01 18:45:20 +0530

    Update play.py

--- EndMusic/play.py
+++ EndMusic/play.py
@@ -9,6 +9,8 @@ async def play_func(_, m: MyHeartYashvi):
         return await m.reply("Looks like you're an anonymous, try without anonymous 🤧")
     if not user_id:
         return
+    fallen = await message.reply("» Processing query...")
+
     administrators = await get_administrators(message.chat)
     chid = message.chat.id
 
@@ -27,13 +29,13 @@ async def play_func(_, m: MyHeartYashvi):
                     invitelink = await _.export_chat_invite_link(chid)
                 except:
                     await fallen.edit(
-                        "<b>» Admin rights required !</b>")
+                        "<b>» Invite members right required to invite music assistant !</b>")
                     return
 
                 try:
-                    await USER.join_chat(invitelink)
-                    await USER.send_message(
-                        message.chat.id, "» Assistant joined !")
+                    await YashuAlpha.join_chat(invitelink)
+                    await YashuAlpha.send_message(
+                        m.chat.id, "» Assistant joined !")
 
                 except UserAlreadyParticipant:
                     pass
@@ -46,3 +48,27 @@ async def play_func(_, m: MyHeartYashvi):
         await fallen.edit(
             f"<i>» Failed to join !</i>\n\Cause : {e}")
         return
+
+    if replied:
+        if replied.audio or replied.voice:
+            omfoo = await replied.reply("**__fetching audio__** 🎧 ")
+            dl = await replied.download()
+            link = replied.link
+            if replied.audio:
+                if replied.audio.title:
+                    songname = replied.audio.title[:70]
+                else:
+                    if replied.audio.file_name:
+                        songname = replied.audio.file_name[:70]
+                    else:
+                        songname = "Audio"
+                dur = replied.audio.duration
+            elif replied.voice:
+                songname = "Voice note"
+                dur = replied.voice.duration
+
+    elif len(m.command) == 2:
+        query = m.text.split(None, 1)[1]
+        omfoo = await m.reply("**__Searching query__**")
+        
+            
