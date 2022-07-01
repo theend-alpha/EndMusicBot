35c96764064ad2365495e5ccd4faff0952087e3e
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-01 18:22:35 +0530

    Update play.py

--- EndMusic/play.py
+++ EndMusic/play.py
@@ -9,4 +9,40 @@ async def play_func(_, m: MyHeartYashvi):
         return await m.reply("Looks like you're an anonymous, try without anonymous 🤧")
     if not user_id:
         return
-    
+    administrators = await get_administrators(message.chat)
+    chid = message.chat.id
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "Anonymous"
+    usar = user
+    wew = usar.id
+    try:
+        await _.get_chat_member(chid, wew)
+    except:
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                try:
+                    invitelink = await _.export_chat_invite_link(chid)
+                except:
+                    await fallen.edit(
+                        "<b>» Admin rights required !</b>")
+                    return
+
+                try:
+                    await USER.join_chat(invitelink)
+                    await USER.send_message(
+                        message.chat.id, "» Assistant joined !")
+
+                except UserAlreadyParticipant:
+                    pass
+                except Exception:
+                    await fallen.edit(
+                        f"<b>» Music player assistant not in this chat, Try: /join")
+    try:
+        await USER.get_chat(chid)
+    except Exception as e:
+        await fallen.edit(
+            f"<i>» Failed to join !</i>\n\Cause : {e}")
+        return
