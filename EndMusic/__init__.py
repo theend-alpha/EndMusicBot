da251772ca76c6bde7dbd6a6e4e33b1abf70f1d7
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-01 18:02:11 +0530

    Update __init__.py

--- EndMusic/__init__.py
+++ EndMusic/__init__.py
@@ -2,3 +2,83 @@ from pyrogram import Client as End, filters
 from pyrogram.types import Message as MyHeartYashvi
 from Config import *
 from helpers import *
+from PIL import Image, ImageDraw, ImageFont
+
+def ytsearch(query: str):
+    try:
+        search = VideosSearch(query, limit=1).result()
+        data = search["result"][0]
+        songname = data["title"]
+        url = data["link"]
+        duration = data["duration"]
+        thumbnail = data["thumbnails"][0]["url"]
+        return [songname, url, duration, thumbnail]
+    except Exception as e:
+        print(e)
+        return 0
+
+
+async def ytdl(format: str, link: str):
+    stdout, stderr = await bash(f'yt-dlp --geo-bypass -g -f "[height<=?720][width<=?1280]" {link}')
+    if stdout:
+        return 1, stdout
+    return 0, stderr
+
+def transcode(filename):
+    ffmpeg.input(filename).output(
+        "input.raw", 
+        format="s16le", 
+        acodec="pcm_s16le", 
+        ac=2, 
+        ar="48k"
+    ).overwrite_output().run()
+    os.remove(filename)
+
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
+
+
+async def generate_cover(thumbnail, title, userid, ctitle):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(f"thumb{userid}.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    image1 = Image.open(f"thumb{userid}.png")
+    images = choice(ZAID_IMGS)
+    image2 = Image.open(images)
+    image3 = changeImageSize(1280, 720, image1)
+    image4 = changeImageSize(1280, 720, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save(f"temp{userid}.png")
+    img = Image.open(f"temp{userid}.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("Process/ImageFont/finalfont.ttf", 60)
+    font2 = ImageFont.truetype("Process/ImageFont/finalfont.ttf", 70)     
+    draw.text((20, 45), f"{title[:30]}...", fill= "white", stroke_width = 1, stroke_fill="white", font=font2)
+    draw.text((120, 595), f"PlAYING ON: {ctitle[:20]}...", fill="white", stroke_width = 1, stroke_fill="white" ,font=font)
+    img.save(f"final{userid}.png")
+    os.remove(f"temp{userid}.png")
+    os.remove(f"thumb{userid}.png") 
+    final = f"final{userid}.png"
+    return final
