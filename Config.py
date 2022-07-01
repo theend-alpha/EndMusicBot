f60a77e0337d6a810f73a1a4096477b91e7cf5b4
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-01 16:07:01 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,13 @@
 import os
 
-API_ID = os.environ.get('API_ID')
+API_ID = int(os.environ.get('API_ID'))
 
 API_HASH = os.environ.get('API_HASH')
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 MONGO_URL = os.environ.get('MONGO_DB_URL')
+
+BOT_USERNAME = os.environ.get('BOT_USERNAME')
+
+
